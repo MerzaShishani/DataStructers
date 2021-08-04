a0a63511090ac31947deb2da7cbf614a39c5aa1c
Merza Shishani <salmerza22@gmail.com>
2021-08-04 23:59:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Array numbers = new Array(3);
+        numbers.insert(1);
+        numbers.insert(1);
+        numbers.insert(1);
+        numbers.insert(1);
+        numbers.print();
+    }
+}
